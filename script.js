7ce6a690ee03f6b4d22592b54f90ba608328e997
Patrick <patrickgonzalesquintana@gmail.com>
2023-04-28 00:47:51 -0400

    Se añadio el archivo script.js y las dos paginas html

---
+++ script.js
@@ -0,0 +1,10 @@
+//console.log("Hola mundo!!!!");
+
+var botonPagina2 = document.getElementById("boton-pagina2");
+
+// Agregar un controlador de eventos al botón
+botonPagina2.addEventListener("click", function() {
+    // Redirigir al usuario a la página 2
+    console.log("Hola mundo2222!!!!");
+    window.location.href = "D:\Descargas\UCB_1_2023\HACKATON\practicas\prueba\pagina2.html";
+});
